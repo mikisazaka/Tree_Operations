e1badccf4f2dbc92fe28087d6828ca7a8c692c66
Graciele Miki Sazaka <gracielesazaka@gmail.com>
2025-10-13 15:25:23 -0300

    feat: Métodos de inserção e busca

--- project/js/Arvore.js
+++ project/js/Arvore.js
@@ -2,4 +2,19 @@ class Arvore {
     constructor() {
         this.raiz = null;
     }
+
+    buscar(no, valor) {
+        if (no === null) return null;
+
+        if (no.valor == valor) return no;
+
+        for (let i = 0; i < no.filhos.length; i++) {
+            const resultado = this.buscar(no.filhos[i], valor);
+            if (resultado !== null) {
+                return resultado;
+            }
+        }
+
+        return null;
+    }
 }
