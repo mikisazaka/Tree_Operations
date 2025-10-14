e688f9bb549069495e3ab02704ba8f5f7ac7c91a
simplicioJoao <simpliciojoaopaulo35@gmail.com>
2025-10-14 08:56:44 -0300

    feat: Método de remoção

--- project/js/Arvore.js
+++ project/js/Arvore.js
@@ -17,4 +17,27 @@ class Arvore {
 
         return null;
     }
+
+    remover(valor, no) {
+        if (no === this.raiz && no.valor === valor) {
+            this.raiz = null;
+            return true;
+        }
+
+        if (no === null) return false;
+
+        for (let i = 0; i < no.filhos.length; i++) {
+            const filho = no.filhos[i];
+
+            if (filho.valor === valor) {
+                const noRemovido = no.filhos[i];
+                no.filhos.splice(i, 1, ...noRemovido.filhos);
+                return true;
+            }
+            if (this.remover(valor, filho)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
